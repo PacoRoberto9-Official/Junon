9cb4d618587475a28edabbeb2bc1952a0f3a9192
akrolpative <guys34577@gmail.com>
2026-09-06 18:00:01 -0700

    lets make /effect not visible to the public but still usable for those who know

--- packages/junon-io/server/commands/effect.js
+++ packages/junon-io/server/commands/effect.js
@@ -7,7 +7,8 @@ class Effect extends BaseCommand {
     return [
       "Gives an effect to an entity",
       "/effect give [player|entity_id] [effectname]",
-      "/effect give [player|entity_id] [effectname] [duration]",
+      // "/effect give [player|entity_id] [effectname] [duration]",
+      // DOESNT FULLY WORK YET, SHOULD BE FIXED BY perrito/akrolpative in later update and fully implemented.
       "/effect clear [player|entity_id] [effectname]",
       "/effect clear [player]",
       "ex: " + this.getAllowedEffects().join(", ")
